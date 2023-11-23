a7e3788238c5f49423d69b0835d6852ca5a33b5b
Pritampk7 <pritamofficial7@gmail.com>
2023-11-23 20:10:55 +0530

    removekthduplicate

---
+++ recap/removeinplaceelement.py
@@ -0,0 +1,13 @@
+class Solution:
+    def remove(self, arr: list[int], target: int) -> int:
+        l = 0
+
+        for r in range(len(arr)):
+            if arr[r] == target:
+                arr[l] = arr[r]
+                l += 1
+        return l
+
+
+obj = Solution()
+print(obj.remove([0, 1, 2, 2, 3, 0, 4, 2], target=2))
